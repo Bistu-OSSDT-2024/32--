5143afdb515324722c68bf924c9553cf8989f82d
23223hdh <145683065+23223hdh@users.noreply.github.com>
2024-06-26 23:42:27 +0800

    Add files via upload

---
+++ 3/requests/api.js
@@ -0,0 +1,6 @@
+const API_TRAIN = "https://www.coze.cn/space/7384329354979098665/bot/7384329348398546981/";
+
+module.exports = {
+    API_TRAIN_SEARCH_ZZ: API_TRAIN+"s2ssearch",
+    API_TRAIN_KEY: "",
+}
